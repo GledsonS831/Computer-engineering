6d7e6e6a762ce4a26faec3f542685bd017968f02
GledsonS831 <glegogle84@gmail.com>
2019-03-19 23:58:49 -0300

    Grafos

--- Graphs/Grafo adjacente nao direcionado/testando.py
+++ Graphs/Grafo adjacente nao direcionado/testando.py
@@ -1,15 +1,19 @@
 from grafo_adj_nao_dir import Grafo
-g = Grafo(['J', 'C', 'E', 'P', 'M', 'T', 'Z'])
+g = Grafo(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'])
 #{'a1':'J-C', 'a2':'C-E', 'a3':'C-E', 'a4':'C-P', 'a5':'C-P', 'a6':'C-M', 'a7':'C-T', 'a8':'M-T', 'a9':'T-Z'}
-g.adicionaAresta('J-C')
-g.adicionaAresta('C-E')
-g.adicionaAresta('C-E')
-g.adicionaAresta('C-P')
-g.adicionaAresta('C-P')
-g.adicionaAresta('C-M')
-g.adicionaAresta('C-T')
-g.adicionaAresta('M-T')
-g.adicionaAresta('T-Z')
-g.adicionaAresta('M-P')
+g.adicionaAresta('a-e')
+g.adicionaAresta('a-d')
+g.adicionaAresta('a-h')
+g.adicionaAresta('a-h')
+g.adicionaAresta('b-c')
+g.adicionaAresta('b-g')
+g.adicionaAresta('b-f')
+g.adicionaAresta('d-e')
+g.adicionaAresta('c-g')
+g.adicionaAresta('a-g')
 
-print(g)
+
+
+g.eh_conexo()
+
+
