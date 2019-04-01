aff53ff6028dac44487be177beedb49a8e49fa5a
GledsonS831 <glegogle84@gmail.com>
2019-03-31 22:51:42 -0300

    Update testando.py

--- Graphs/Caminho euleriano/testando.py
+++ Graphs/Caminho euleriano/testando.py
@@ -2,7 +2,7 @@ from grafo_adj_nao_dir import Grafo
 
 
 g = Grafo([], [])
-for i in ['J', 'C', 'E', 'P', 'M', 'T', 'Z']:
+for i in ['J', 'C', 'E', 'P', 'M', 'T']:
     g.adiciona_vertice(i)
 
 for i in ['J-C', 'C-E', 'C-E', 'C-P', 'C-P', 'C-M', 'C-T', 'M-T']:
