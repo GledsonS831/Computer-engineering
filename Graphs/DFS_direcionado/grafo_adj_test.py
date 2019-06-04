e7165215e6fa03b2c103feea95d042e89dc192f9
GledsonS831 <glegogle84@gmail.com>
2019-06-04 00:24:36 -0300

    Grafos

---
+++ Graphs/DFS_direcionado/grafo_adj_test.py
@@ -0,0 +1,38 @@
+import unittest
+from grafo_adj import *
+
+
+
+class TestGrafo(unittest.TestCase):
+
+    def setUp(self):
+
+        # Grafo da Paraíba
+        self.g_p = Grafo([], [])
+        for i in ['J', 'C', 'E', 'P', 'M', 'T', 'Z']:
+            self.g_p.adiciona_vertice(i)
+        for i in ['J-C', 'C-E', 'C-E', 'C-P', 'C-P', 'C-M', 'C-T', 'M-T', 'T-Z']:
+            self.g_p.adiciona_aresta(i)
+
+        # Grafo da Paraíba sem arestas paralelas
+        self.g_p_sem_paralelas = Grafo([], [])
+        for i in ['J', 'C', 'E', 'P', 'M', 'T', 'Z']:
+            self.g_p_sem_paralelas.adiciona_vertice(i)
+        for i in ['J-C', 'C-E', 'C-P', 'C-M', 'C-T', 'M-T', 'T-Z']:
+            self.g_p_sem_paralelas.adiciona_aresta(i)
+        self.carga_gp = ['M', 'J']
+        # Grafos completos
+        self.g_c = Grafo([], [])
+        for i in ['J', 'C', 'E', 'P']:
+            self.g_c.adiciona_vertice(i)
+        for i in ['J-C', 'J-E', 'J-P', 'C-J', 'C-E', 'C-P', 'E-J', 'E-C', 'E-P', 'P-J', 'P-C', 'P-E']:
+            self.g_c.adiciona_aresta(i)
+
+        self.cargag_c = ['C', 'P']
+        self.g_c3 = Grafo([], [])
+        self.g_c3.adiciona_vertice('J')
+
+    def dfs(self, ini):
+        pass
+
+
