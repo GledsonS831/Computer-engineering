d5f67172fb754dc38f367b83a84cfb6887cc621c
GledsonS831 <glegogle84@gmail.com>
2019-06-05 19:20:01 -0300

    POO

--- POO/ProjBanco/src/main/java/br/edu/ifpb/EntradaInvalidaException.java
+++ POO/ProjBanco/src/main/java/br/edu/ifpb/EntradaInvalidaException.java
@@ -1,8 +1,6 @@
 package br.edu.ifpb;
 
 import java.io.IOException;
-import java.lang.annotation.Annotation;
-import java.util.Scanner;
 
 public class EntradaInvalidaException extends Exception {
 
--- POO/ProjBanco/src/main/java/br/edu/ifpb/SaldoInsuficienteException.java
+++ POO/ProjBanco/src/main/java/br/edu/ifpb/SaldoInsuficienteException.java
@@ -1,6 +1,5 @@
 package br.edu.ifpb;
 
-import java.util.Scanner;
 
 public class SaldoInsuficienteException extends Exception{
     @Override
