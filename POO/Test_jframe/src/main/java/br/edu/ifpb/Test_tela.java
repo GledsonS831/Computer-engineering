b385d6cc37edc148bbe7dfc9693b6cf6be99025e
GledsonS831 <glegogle84@gmail.com>
2019-06-09 22:21:25 -0300

    POO

---
+++ POO/Test_jframe/src/main/java/br/edu/ifpb/Test_tela.java
@@ -0,0 +1,17 @@
+package br.edu.ifpb;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+import java.io.File;
+
+
+public class Test_tela {
+
+    public static void main(String[] args ) throws Exception {
+        Windows_main wd = new Windows_main();
+        wd.exe(true);
+    }
+}
