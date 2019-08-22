837a7bf9b6d1b3731ec060c536fc6e3512b8b469
GLEDSONS831 <glegogle84@gmail.com>
2019-08-21 22:02:21 -0300

    Atividades em java

--- POO/Pratica2/Exercicio8/Data.java
+++ POO/Pratica2/Exercicio8/Data.java
@@ -38,4 +38,17 @@ public class Data {
         }
         return sd+"/"+sm+"/"+zeros+sa;
     }
+    public boolean isBissexto(){
+        if(ano % 4 == 0 && ano % 100 != 0){
+            return true;
+        }
+        else{
+            if(ano % 400 == 0){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+    }
 }
--- POO/Pratica2/Exercicio8/TestaData.java
+++ POO/Pratica2/Exercicio8/TestaData.java
@@ -4,6 +4,8 @@ public class TestaData {
         System.out.println(d1.getDia()+" "+d1.getMes()+" "+d1.getAno());
         System.out.println(d1.getMesExtenso());
         Data d2 = new Data(01, 04, -9);
+        Data d3 = new Data(14, 8, 2020);
         System.out.println(d2.getMesExtenso());
+        System.out.println(d3.isBissexto());
     }
 }
